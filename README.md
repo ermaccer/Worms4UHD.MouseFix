4fb267b7b39dfa75c957cc68b964258218ebdf1d
ermaccer <40604575+ermaccer@users.noreply.github.com>
2021-07-18 14:16:27 +0200

    upload files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Worms Ultimate Mayhem Mouse Fix
+This is a simple plugin which aims to fix mouse movement, in vanilla
+game cursor jumps around position and is overall stuttery.
+
+
+## Possible Reason
+
+This plugin disables one function which sets the cursor position with
+system call "SetCursorPos" and also to internal game position, there is another function which also sets
+cursor position but this one only sets the position to internal game position.
+
+It looks like there might be some kind of race error between these two functions, disabling
+the system one seems to fix the cursor stuttering.
--- Worms4UHD.MouseFix/dllmain.cpp
+++ Worms4UHD.MouseFix/dllmain.cpp
@@ -1,5 +1,18 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include "MemoryMgr.h"
+#include <Windows.h>
+
+using namespace Memory::VP;
+
+void WINAPI SetMousePosDirectly(int ptr, int X, int Y)
+{
+}
+
+void Init()
+{
+	InjectHook(0x701D92, SetMousePosDirectly, PATCH_JUMP);
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,6 +22,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+		Init();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
