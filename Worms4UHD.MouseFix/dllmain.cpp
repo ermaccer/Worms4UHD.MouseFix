fb65c275ebb7e4619a093abdd13d0ab9728dc76e
ermaccer <40604575+ermaccer@users.noreply.github.com>
2023-03-22 20:41:06 +0100

    condensed & updated vs

--- Worms4UHD.MouseFix/dllmain.cpp
+++ Worms4UHD.MouseFix/dllmain.cpp
@@ -4,72 +4,19 @@
 
 using namespace Memory::VP;
 
-typedef int(__cdecl* tsub_701348)(int a1);
-tsub_701348 sub_701348 = (tsub_701348)0x701348;
-typedef int(__cdecl* tsub_6405AC)();
-tsub_6405AC sub_6405AC = (tsub_6405AC)0x6405AC;
-
-DWORD isCursorVisible = 0;
-
-int WINAPI SetMousePosDirectly(int ptr, int X, int Y)
+void WINAPI SetMousePosDirectly(int ptr, int X, int Y)
 {
-    if (*(BYTE*)isCursorVisible)
-    {
-        return 0;
-    }
+    bool& bCursorVisible = *(bool*)0x97AC96;
+    if (bCursorVisible)
+        return;
 
-    struct tagPOINT Point; // [sp+Ch] [bp-8h]@1
-    GetCursorPos(&Point);
-
-    int v3; // eax@3
-    int v4; // esi@3
-    HWND v5; // eax@4
-    HWND v6; // eax@4
-    struct tagPOINT v8; // [sp+4h] [bp-10h]@4
-
-    if (Point.x != X || Point.y != Y)
-    {
-        v3 = (*(int(__stdcall**)(int, signed int))(*(DWORD*)ptr + 60))(ptr, 1);
-        v4 = sub_701348(v3);
-        if (v4)
-        {
-            v8.y = Y;
-            v8.x = X;
-            v5 = (HWND)sub_6405AC();
-            ScreenToClient(v5, &v8);
-            v6 = (HWND)sub_6405AC();
-            ScreenToClient(v6, &Point);
-            *(DWORD*)(v4 + 176) += v8.x - Point.x;
-            *(DWORD*)(v4 + 180) += v8.y - Point.y;
-        }
-        #ifdef DEBUG
-            printf("set cursor (%d, %d) to pos %d %d\n", Point.x, Point.y, X, Y);
-        #endif 
-        SetCursorPos(X, Y);
-    }
-
-    return 0;
+    ((void(_stdcall*)(int, int, int))0x701D92)(ptr, X, Y);
 }
+
 
 void Init()
 {
-    #ifdef DEBUG
-        AllocConsole();
-        freopen("conin$", "r", stdin);
-        freopen("conout$", "w", stdout);
-        freopen("conout$", "w", stderr);
-        printf("Debugging Window enabled\n");
-    #endif 
-
-    DWORD Base = (DWORD)GetModuleHandleA("WormsMayhem.exe");
-    isCursorVisible = Base + 0x57AC96;
-
-    #ifdef DEBUG
-        printf("InGame %x\n", inGame);
-    #endif 
-
-
-    InjectHook(0x701D92, SetMousePosDirectly, PATCH_JUMP);
+    Patch(0x89AE5C, &SetMousePosDirectly);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
